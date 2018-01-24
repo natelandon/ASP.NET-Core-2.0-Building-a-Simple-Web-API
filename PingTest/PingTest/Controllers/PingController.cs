301ab36c1f285cbd5caa9f4451f1127b9e722260
Nate Landon <druidprogramming@gmail.com>
2018-01-23 21:58:26 -0700

    Add files via upload

---
+++ PingTest/PingTest/Controllers/PingController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PingTest.Controllers
+{
+    public class PingController : Controller
+    {
+        [HttpGet]
+        [Route("api/ping")]
+        public IActionResult Ping()
+        {
+            return Ok("pong!!");
+        }
+    }
+}
